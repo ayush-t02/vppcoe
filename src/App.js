7e131e8dac4702e93ad0b451c63f1918a7974675
Ayush Tripathi <tripathiayush23@gmail.com>
2023-03-29 15:01:56 +0530

    mui version added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CreateMui from "./components/CreateMui";
 import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
     <>
       <CreatePost />
+      <CreateMui />
     </>
   );
 }
--- src/components/CreateMui.js
+++ src/components/CreateMui.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 const CreateMui = () => {
+  const [title, setTitle] = new useState("");
+  const [caption, setCaption] = new useState("");
+  const [img, setImage] = new useState(null);
+
+  console.log(title);
+  console.log(caption);
+  console.log(img);
+
   return (
     <>
       <Box
@@ -20,6 +28,7 @@ const CreateMui = () => {
             multiline
             rows={1}
             defaultValue='Enter the title'
+            onChange={setTitle}
           />
           <TextField
             id='outlined-multiline-static'
@@ -27,8 +36,16 @@ const CreateMui = () => {
             multiline
             rows={4}
             defaultValue='Enter the caption'
+            onChange={setCaption}
           />
-          <input id='fileInput' type='file' />
+          <input
+            id='fileInput'
+            type='file'
+            onChange={(event) => {
+              let file = event.target.files[0];
+              setImage(URL.createObjectURL(file));
+            }}
+          />
         </div>
       </Box>
     </>
